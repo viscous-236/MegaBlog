e54156a1c27059a435e04f325e87416c6576270e
Vaibhav <vaibhavgoyal2310066@gmail.com>
2025-01-18 02:39:11 +0530

    button erroe

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -10,7 +10,8 @@ function Button({
 }) {
     return (
         <button
-        className={`px-4  py-2 rounded-lg ${bgColor} ${textColor} ${className} {...props}`}
+        className={`px-4  py-2 rounded-lg ${bgColor} ${textColor} ${className}`}
+        {...props}
         >{children}</button>
     )
 }
